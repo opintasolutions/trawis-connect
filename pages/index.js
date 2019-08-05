830ad1cbabae7e665b7fa5af62d639cf8f397a68
CoderPrans <pranavbhaskar17@gmail.com>
2019-08-05 22:55:19 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,60 @@
+import fetch from 'isomorphic-unfetch';
+import {useState, useEffect} from 'react';
+
+const graphqlQuery = `
+  {
+    users {
+      name
+    } 
+  }
+`;
+
+const Index = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`api/graphql`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({query: graphqlQuery}),
+    })
+      .then(res => res.json())
+      .then(json => {
+        const {
+          data: {users},
+        } = json;
+        setUsers(users);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      {users.length ? (
+        users.map((user, i) => <div key={i}>{user.name}</div>)
+      ) : (
+        <p>Loading....</p>
+      )}
+    </div>
+  );
+};
+
+// Index.getInitialProps = async () => {
+//   const response = await fetch('http://localhost:3000/api/graphql', {
+//     method: 'POST',
+//     headers: {
+//       'Content-type': 'application/json',
+//     },
+//     body: JSON.stringify({query: '{ users { name } }'}),
+//   });
+
+//   const {
+//     data: {users},
+//   } = await response.json();
+
+//   return {users};
+// };
+
+export default Index;
